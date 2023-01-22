6055c8ef977c3ad693ecd732220b6ec025448e6f
Gaurav Sahal <gaurav.sahal10@gmail.com>
2023-01-22 12:01:01 +0530

    First commit

---
+++ src/com/gaurav/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.gaurav.springdemo.dao;
+
+import java.util.List;
+
+import com.gaurav.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+}
