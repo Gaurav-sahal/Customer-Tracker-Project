7685231ee7da3b5e1e839339023f848b8fef2c09
Gaurav Sahal <gaurav.sahal10@gmail.com>
2023-01-30 22:07:13 +0530

    Update Dao directory

--- src/main/java/com/gaurav/springdemo/dao/CustomerDAO.java
+++ src/main/java/com/gaurav/springdemo/dao/CustomerDAO.java
@@ -6,7 +6,13 @@ import com.gaurav.springdemo.entity.Customer;
 
 public interface CustomerDAO {
 
-	public List<Customer> getCustomers();
+	public List<Customer> getCustomers(int theSortField);
 
 	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+
+	public List<Customer> searchCustomers(String theSearchName);
 }
