b5b3335a7370993150a855df749e118dd82c459a
Gaurav Sahal <gaurav.sahal10@gmail.com>
2023-03-29 23:12:28 +0530

    Relocated Customer-Tracker-Project

--- src/main/java/com/gaurav/springdemo/utility/SortUtils.java
+++
@@ -1,8 +0,0 @@
-package com.gaurav.springdemo.utility;
-
-public interface SortUtils {
-
-	public static final int FIRST_NAME = 1;
-	public static final int LAST_NAME = 2;
-	public static final int EMAIL = 3;
-}
